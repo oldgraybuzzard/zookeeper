c63626a299f1bf99faa15d7d1095d6368e6aa063
Kendall Felder <61666382+oldgraybuzzard@users.noreply.github.com>
2021-10-29 11:27:16 -0400

    completed with the front end development

--- server.js
+++ server.js
@@ -5,21 +5,11 @@ const { animals } = require('./data/animals.json');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-
+app.use(express.static('public'));
 //parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
 //parse incoming JSON data
 app.use(express.json());
-
-
-
-
-
-
-
-
-
-
 
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
@@ -108,8 +98,6 @@ app.get('/api/animals/:id', (req, res) => {
     }
 });
 
-
-
 app.post('/api/animals', (req, res) => {
   // set id based on what the next index of the array will be
   req.body.id = animals.length.toString();
@@ -122,6 +110,23 @@ app.post('/api/animals', (req, res) => {
     res.json(animal);
   }
 });
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/index.html'));
+});
+
+app.get('/animals', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/animals.html'));
+});
+
+app.get('/zookeepers', (reg, res) => {
+  res.sendFile(path.join(__dirname, 'public/zookeepers.html'));
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/index.html'));
+});
+
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
